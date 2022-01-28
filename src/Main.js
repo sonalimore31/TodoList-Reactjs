49a3a92de7734fc9d4ff73ba80934c7bd66d44d4
sonali <sonalimore773@gmail.com>
2022-01-28 12:34:56 +0530

    Delete Task Functionality done and Project Completed

--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,25 @@
-import React from "react"
+import React, { useState } from "react"
 
 const Main = () => {
+  const [taskname, settaskname] = useState("")
+  const [tasklist, settasklist] = useState([])
+  function addTask() {
+    settasklist([...tasklist, taskname])
+  }
+  const tasklistcontent = tasklist.map((task, index) => {
+    return (
+      <div>
+        <p>{task}</p>
+        <i class='far fa-trash-alt' onClick={() => deletetask(index)}></i>
+      </div>
+    )
+  })
+
+  function deletetask(index) {
+    var duparray = [...tasklist]
+    duparray.splice(index, 1)
+    settasklist(duparray)
+  }
   return (
     <div>
       <div className='row justify-content-center'>
@@ -10,8 +29,16 @@ const Main = () => {
             type='text'
             placeholder='Enter Task'
             className='form-control'
+            vlaue={taskname}
+            onChange={(e) => {
+              settaskname(e.target.value)
+            }}
           />
-          <button className='btn btn-success'>ADD</button>
+          <button className='btn btn-success' onClick={addTask}>
+            ADD
+          </button>
+          <br />
+          {tasklistcontent}
         </div>
       </div>
     </div>
